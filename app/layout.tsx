f4539c6554ce96005b1565fce2508832c8627baa
JM Fantin <jmfantin@outlook.com>
2023-11-04 21:36:29 -0300

    some routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen w-full items-center justify-center">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
