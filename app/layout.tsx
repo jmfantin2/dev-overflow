3b9343c4a2e199e634a7686733e3c768e9a53908
JM Fantin <jmfantin@outlook.com>
2023-11-06 13:56:10 -0300

    attempt to fix middleware failure by downgrading next and react

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
+// eslint-disable-next-line no-unused-vars, camelcase
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dev Overflow',
+  description:
+    'A community-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
+};
 
 export default function RootLayout({
   children,
