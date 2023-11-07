8ae3fe836a1fafced662132e0644a8a498fe97e0
JM Fantin <jmfantin@outlook.com>
2023-11-07 14:10:36 -0300

    sync

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from '@clerk/nextjs';
 
-export default function HomePage() {
+export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
