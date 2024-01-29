78e1c823f642a4de221837aa69c4215ea75dc799
JM FANTIN <jmfantin@outlook.com>
2024-01-29 15:49:29 -0300

    add zod form

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,5 +1,118 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { QuestionsSchema } from '@/lib/validations';
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+});
+
 export function QuestionForm() {
-  return <div>Questions</div>;
+  //1. Define your form.
+  const form = useForm<z.infer<typeof QuestionsSchema>>({
+    resolver: zodResolver(QuestionsSchema),
+    defaultValues: {
+      title: '',
+      explanation: '',
+      tags: [],
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className=" flex w-full flex-col gap-10"
+      >
+        <FormField //QUESTION TITLE
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
+                person.
+              </FormDescription>
+              <FormMessage className="text-rose-500 opacity-70" />
+            </FormItem>
+          )}
+        />
+        <FormField //DETAILED EXPLANATION
+          control={form.control}
+          name="explanation"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem{' '}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                {/* TODO: ADD AN EDITOR */}
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
+              </FormDescription>
+              <FormMessage className="text-rose-500 opacity-70" />
+            </FormItem>
+          )}
+        />
+        <FormField //TAGS
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  placeholder="Add tags..."
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage className="text-rose-500 opacity-70" />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
---
+++ jmfantin/notes.ts
@@ -0,0 +1,170 @@
+/**
+ * ! COURSE
+ * * https://courses.jsmastery.pro/course/ultimate-next-js-13-course-ebook
+ * * https://github.com/adrianhajdin/stack_overflow_nextjs13/blob/main/package.json
+ *
+ * ? NOTES
+ *
+ * * client_v_server.png
+ * "If search engine optimization, security concerns and user device specifications are NOT a priority for you, and your focus is primarily on delivering dynamic interactivity to the user, then client-side rendering (CSR) with technologies like ReactJS can be a suitable choice. A use case where this approach is applicable is in the B2B domain. In such cases, the target audience is specific and known, eliminating the need to optimize SEO since the product is not intended for a wide public audience. This allows you to prioritize developing interactive features and functionalities without dedicating significant resources to SEO optimization."
+ *
+ * ? STRUCTURE
+ *
+ * * @/lib/actions
+ * a connection between backend and frontend
+ *
+ * * components/ui folder
+ * shadcn components installed via 'npx shadcn-ui@latest add COMP_NAME'
+ *
+ * ? PROGRESS
+ * * INTRO
+ * / Intro
+ * * PREREQUISITES
+ * / Prerequisites
+ * * INTRODUCTION TO NEXT.JS
+ * / Before Next.js
+ * / Introduction to Next.js
+ * / How Web Works
+ * * INTRODUCTION TO NEXT.JS
+ * / Before Next.js
+ * / Introduction to Next.js
+ * / How Web Works
+ * * SETUP
+ * / DevFlow Project Setup
+ * / Eslint Prettier Setup
+ * / Git/Github Setup
+ * / Tailwind CSS Setup
+ * / Code Architecture
+ * * NEXT.JS ROUTING
+ * / What is Next.js Routing
+ * / Creating Routes for DevFlow
+ * * NEXT.JS ARCHITECTURE
+ * / The Modern Auth Service
+ * / Setup Auth for DevFlow
+ * * LAYOUTS
+ * / Creating Layouts Using Next Font and Metadata in DevFlow
+ * * THEME
+ * / Creating a global theme context for DevFlow
+ * * NAVBAR
+ * / Container and user account
+ * / Shadcn installation
+ * / Theme Switcher and Mobile Navigation
+ * * SIDEBAR
+ * / Left Sidebar Component
+ * / Right Sidebar Component
+ * * HOME PAGE
+ * / Home Route
+ * / Local Search Bar Component
+ * / Home Filters
+ * / Create a Question Card
+ * TODO: ASK A QUESTION PAGE
+ * / Question Form and Validations
+ * ! The Question Editor
+ * / Custom Multiple Tags Input
+ * / Making the Form Reusable
+ * TODO: DEVELOP BACKEND
+ * / Thinking in Backend
+ * / MongoDB and Actions Server Setup
+ * / Creating a Question Model
+ * / Creating a User Model
+ * / Creating a Tag Model
+ * TODO: DEVELOP BACKEND
+ * / Thinking in Backend
+ * / MongoDB and Actions Server Setup
+ * / Creating a Question Model
+ * / Creating a User Model
+ * / Creating a Tag Model
+ * TODO: CREATE A QUESTION
+ * / Create Question Action
+ * TODO: FETCHING QUESTIONS ON THE HOME PAGE
+ * / Display Questions on the Home Page
+ * TODO: THE WEBHOOKS
+ * / Why and what are Webhooks
+ * / implement Webhooks and User Actions
+ * / Deploy Webhooks
+ * TODO: COMMUNITY PAGE
+ * / Create Community Page
+ * TODO: TAGS PAGE
+ * / Create Tags Page
+ * TODO: QUESTION DETAILS
+ * / Create Question Details Page
+ * / Parse and Display Question Content
+ * / Create Answer Form
+ * / Create Answer Model
+ * / Implement Create Answer Action
+ * / Integrate Create Answer Action Inside Answer Form
+ * / Display All Answers
+ * TODO: VOTING
+ * / Create Votes UI
+ * / Create Upvote/Downvote Actions for Question
+ * / Integrate Question Upvote/Downvote Actions on UI
+ * / Create Answer Voting
+ * TODO: COLLECTIONS PAGE
+ * / Implement Save Question and Create Collection Page
+ * / Display all saved questions
+ * TODO: VIEWS
+ * / Create Question Details Page View
+ * TODO: TAG DETAILS PAGE
+ * / Create a Tag Details Page
+ * TODO: PROFILE PAGE
+ * / Create Profile Page
+ * / Create User Stats UI
+ * / Implement User Questions Tab
+ * / Implement User Answers Tab
+ * TODO: EDIT/DELETE USER ACTIONS
+ * / Implement Edit/Delete Answer Component
+ * / Create Edit Question Page
+ * / Create Edit Profile Page
+ * TODO: SHOW TOP RESULTS
+ * / Show Top Questions
+ * / Show Popular Tags
+ * TODO: THE LOCAL SEARCH FUNCTIONALITY
+ * / Manage Search State
+ * / Implement Search Functionality for Home Page
+ * / Implement Search Functionality for Community Page
+ * / Implement Search Functionality for Collection Page
+ * / Implement Search Functionality for Tags Page
+ * TODO: THE FILTERS
+ * / Manage Filter State
+ * / Integrate Filters on Home Page
+ * / Integrate Filters on Community Page
+ * / Integrate Filters on Collection Page
+ * / Integrate Filters on Tags Page
+ * TODO: THE PAGINATION
+ * / Create Pagination Component
+ * / Implement Pagination Component
+ * / Implement Pagination on Home Page
+ * / Implement Pagination on the rest of the pages
+ * TODO: GLOBAL SEARCH
+ * / Create Global Search UI
+ * / Create GlobalSearch Result Component
+ * / Create Global Search Filters
+ * / Implement the GlobalSearch Actions
+ * TODO: REPUTATION
+ * / What is Reputation and how to approach it
+ * / Implement Reputation points for questions
+ * / Implement Reputation points for answers
+ * / More on Reputation and how to extend it
+ * TODO: BADGE SYSTEM
+ * / Implement the Badge System
+ * TODO: Generate AI Answer
+ * / Setup AI Answer Feature
+ * / Implement API Route for the AI feature
+ * / Display AI results on the UI
+ * TODO: LOADING AND TOASTS
+ * / Create a Loading State for the Home Page
+ * / Create a Loading State for the Community Page
+ * / Create Loading States for the rest of the pages
+ * / Create Toasts for a few actions
+ * TODO: METADATA
+ * / What is Metadata and how to implement it
+ * TODO: BUG FIXING AND RECOMMENDATION
+ * / Fix bugs and implement recommendations
+ * TODO: NEXT.JS 13.5+
+ * / Upgrade Next.js to the latest version
+ * TODO: DEPLOYMENT
+ * / Deploy the application
+ * TODO: JOBS FEATURE
+ * / A New Approach to Learning your Ultimate Next.js 13 Challenge
+ * / Put Your Skills to the Test
+ */
