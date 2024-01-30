b4e37812ab5fc7213ad5d4b6f693a4977826c1ca
JM FANTIN <jmfantin@outlook.com>
2024-01-30 11:34:48 -0300

    tinymce editor

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,5 +1,6 @@
 'use client';
-
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
@@ -24,6 +25,7 @@ const formSchema = z.object({
 });
 
 export function QuestionForm() {
+  const editorRef = useRef(null);
   //1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -78,7 +80,42 @@ export function QuestionForm() {
                 <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                {/* TODO: ADD AN EDITOR */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) =>
+                    //@ts-ignore
+                    (editorRef.current = editor)
+                  }
+                  initialValue="<p>This is the initial content of the editor.</p>"
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesample',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table',
+                    ],
+                    toolbar:
+                      'undo redo | ' +
+                      'codesample | bold italic forecolor | alignleft aligncenter |' +
+                      'alignright alignjustify | bullist numlist',
+                    content_style: 'body { font-family:Inter; font-size:16px }',
+                    //skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                    //content_css: mode === 'dark' ? 'dark' : 'light',
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you put in the title.
