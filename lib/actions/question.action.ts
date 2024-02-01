dd9eadd129986c68f4618c9ca8990f2cca71641c
JM FANTIN <jmfantin@outlook.com>
2024-02-01 11:19:23 -0300

    actions config

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,10 @@
+'use server';
+
+export async function createQuestion(params: any) {
+  // eslint-disable-next-line no-empty
+  try {
+    // connect to db
+  } catch (err) {
+    // error handling
+  }
+}
