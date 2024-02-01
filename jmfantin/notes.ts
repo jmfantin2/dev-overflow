3262090e145107236221258b7d12553ff5c7f3ed
JM FANTIN <jmfantin@outlook.com>
2024-02-01 14:01:41 -0300

    declare models

--- jmfantin/notes.ts
+++ jmfantin/notes.ts
@@ -71,102 +71,101 @@
  * / The Question Editor
  * / Custom Multiple Tags Input
  * / Making the Form Reusable
- * TODO: DEVELOP BACKEND
+ * * DEVELOP BACKEND
  * / Thinking in Backend
- * ? MongoDB and Actions Server Setup
+ * / MongoDB and Actions Server Setup
  * / Creating a Question Model
  * / Creating a User Model
  * / Creating a Tag Model
  * TODO: CREATE A QUESTION
- * / Create Question Action
+ * ? Create Question Action [24.08]
  * TODO: FETCHING QUESTIONS ON THE HOME PAGE
- * / Display Questions on the Home Page
+ * / Display Questions on the Home Page [15:58]
  * TODO: THE WEBHOOKS
- * / Why and what are Webhooks
- * / implement Webhooks and User Actions
- * / Deploy Webhooks
+ * / Why and what are Webhooks [3:00]
+ * / Implement Webhooks and User Actions [16:11]
+ * / Deploy Webhooks [29:21]
  * TODO: COMMUNITY PAGE
- * / Create Community Page
+ * / Create Community Page [22:51]
  * TODO: TAGS PAGE
- * / Create Tags Page
+ * / Create Tags Page [9:00]
  * TODO: QUESTION DETAILS
- * / Create Question Details Page
- * / Parse and Display Question Content
- * / Create Answer Form
- * / Create Answer Model
- * / Implement Create Answer Action
- * / Integrate Create Answer Action Inside Answer Form
- * / Display All Answers
+ * / Create Question Details Page [14:51]
+ * / Parse and Display Question Content [13:41]
+ * / Create Answer Form [15:09]
+ * / Create Answer Model [4:08]
+ * / Implement Create Answer Action [4:01]
+ * / Integrate Create Answer Action Inside Answer Form [10:47]
+ * / Display All Answers [21:33]
  * TODO: VOTING
- * / Create Votes UI
- * / Create Upvote/Downvote Actions for Question
- * / Integrate Question Upvote/Downvote Actions on UI
- * / Create Answer Voting
+ * / Create Votes UI [9:52]
+ * / Create Upvote/Downvote Actions for Question [6:56]
+ * / Integrate Question Upvote/Downvote Actions on UI [11:33]
+ * / Create Answer Voting [9:35]
  * TODO: COLLECTIONS PAGE
- * / Implement Save Question and Create Collection Page
- * / Display all saved questions
+ * / Implement Save Question and Create Collection Page [5:33]
+ * / Display all saved questions [12:20]
  * TODO: VIEWS
- * / Create Question Details Page View
+ * / Create Question Details Page View [14:21]
  * TODO: TAG DETAILS PAGE
- * / Create a Tag Details Page
+ * / Create a Tag Details Page [17:51]
  * TODO: PROFILE PAGE
- * / Create Profile Page
- * / Create User Stats UI
- * / Implement User Questions Tab
- * / Implement User Answers Tab
+ * / Create Profile Page [25:58]
+ * / Create User Stats UI [9:06]
+ * / Implement User Questions Tab [8:46]
+ * / Implement User Answers Tab [14:29]
  * TODO: EDIT/DELETE USER ACTIONS
- * / Implement Edit/Delete Answer Component
- * / Create Edit Question Page
- * / Create Edit Profile Page
+ * / Implement Edit/Delete Answer Component [16:36]
+ * / Create Edit Question Page [15:29]
+ * / Create Edit Profile Page [22:57]
  * TODO: SHOW TOP RESULTS
- * / Show Top Questions
- * / Show Popular Tags
+ * / Show Top Questions [3:37]
+ * / Show Popular Tags [3:43]
  * TODO: THE LOCAL SEARCH FUNCTIONALITY
- * / Manage Search State
- * / Implement Search Functionality for Home Page
- * / Implement Search Functionality for Community Page
- * / Implement Search Functionality for Collection Page
- * / Implement Search Functionality for Tags Page
+ * / Manage Search State [22:11]
+ * / Implement Search Functionality for Home Page [6:48]
+ * / Implement Search Functionality for Community Page [3:15]
+ * / Implement Search Functionality for Collection Page [1:33]
+ * / Implement Search Functionality for Tags Page [4:23]
  * TODO: THE FILTERS
- * / Manage Filter State
- * / Integrate Filters on Home Page
- * / Integrate Filters on Community Page
- * / Integrate Filters on Collection Page
- * / Integrate Filters on Tags Page
+ * / Manage Filter State [6:24]
+ * / Integrate Filters on Home Page [4:45]
+ * / Integrate Filters on Community Page [6:41]
+ * / Integrate Filters on Collection Page [3:44]
+ * / Integrate Filters on Tags Page [8:31]
  * TODO: THE PAGINATION
- * / Create Pagination Component
- * / Implement Pagination Component
- * / Implement Pagination on Home Page
- * / Implement Pagination on the rest of the pages
+ * / Create Pagination Component [10:39]
+ * / Implement Pagination on Home Page [5:41]
+ * / Implement Pagination on the rest of the pages [22:03]
  * TODO: GLOBAL SEARCH
- * / Create Global Search UI
- * / Create GlobalSearch Result Component
- * / Create Global Search Filters
- * / Implement the GlobalSearch Actions
+ * / Create Global Search UI [9:09]
+ * / Create GlobalSearch Result Component [19:28]
+ * / Create Global Search Filters [9:21]
+ * / Implement the GlobalSearch Actions [27:25]
  * TODO: REPUTATION
- * / What is Reputation and how to approach it
- * / Implement Reputation points for questions
- * / Implement Reputation points for answers
- * / More on Reputation and how to extend it
+ * / What is Reputation and how to approach it [1:41]
+ * / Implement Reputation points for questions [8:31]
+ * / Implement Reputation points for answers [6:02]
+ * / More on Reputation and how to extend it [1:08]
  * TODO: BADGE SYSTEM
- * / Implement the Badge System
+ * / Implement the Badge System [15:55]
  * TODO: Generate AI Answer
- * / Setup AI Answer Feature
- * / Implement API Route for the AI feature
- * / Display AI results on the UI
+ * / Setup AI Answer Feature [5:40]
+ * / Implement API Route for the AI feature [8:45]
+ * / Display AI results on the UI [5:39]
  * TODO: LOADING AND TOASTS
- * / Create a Loading State for the Home Page
- * / Create a Loading State for the Community Page
- * / Create Loading States for the rest of the pages
- * / Create Toasts for a few actions
+ * / Create a Loading State for the Home Page [9:50]
+ * / Create a Loading State for the Community Page [4:18]
+ * / Create Loading States for the rest of the pages [2:14]
+ * / Create Toasts for a few actions [8:45]
  * TODO: METADATA
- * / What is Metadata and how to implement it
+ * / What is Metadata and how to implement it [5:06]
  * TODO: BUG FIXING AND RECOMMENDATION
- * / Fix bugs and implement recommendations
+ * / Fix bugs and implement recommendations [18:48]
  * TODO: NEXT.JS 13.5+
- * / Upgrade Next.js to the latest version
+ * / Upgrade Next.js to the latest version [3:52]
  * TODO: DEPLOYMENT
- * / Deploy the application
+ * / Deploy the application [21:27]
  * TODO: JOBS FEATURE
  * / A New Approach to Learning your Ultimate Next.js 13 Challenge
  * / Put Your Skills to the Test
